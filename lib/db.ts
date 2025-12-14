c6a8384e1189506b8e5e55a6a684865f7c48cfad
Batyrbek Imamidinov <imamidinovbatyrbek@gmail.com>
2025-12-14 13:09:19 +0800

    Install Neon and Prisma

---
+++ lib/db.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@/src/generated/client";
+
+const prisma = new PrismaClient({} as any)
+export default prisma;
+
+declare global {
+    var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient({} as any);
+
+if (process.env.NODE_ENV !== "production") {
+    globalThis.prisma = db;
+}
